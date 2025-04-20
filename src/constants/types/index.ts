1aea32f03bd29f048c04cf91c3dcd2df10d14787
Rommel <137849625+47roms16@users.noreply.github.com>
2025-04-20 13:59:38 +0800

    feat(types): define structure types for SpaceData and related types

--- src/constants/types/index.ts
+++ src/constants/types/index.ts
@@ -2,3 +2,35 @@ export interface NavLinkType {
   label: string;
   href: string;
 }
+
+interface Image {
+  png: string;
+  webp: string;
+}
+
+export interface Destination {
+  name: string;
+  images: Image;
+  description: string;
+  distance: string;
+  travel: string;
+}
+
+export interface Crew {
+  name: string;
+  images: Image;
+  role: string;
+  bio: string;
+}
+
+export interface Technology {
+  name: string;
+  images: Image;
+  description: string;
+}
+
+export interface SpaceData {
+  destinations: Destination[];
+  crew: Crew[];
+  technology: Technology[];
+}
