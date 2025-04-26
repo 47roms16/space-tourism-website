af2da9d1f597f958c9c949d4c4817f61cb92037e
Rommel <137849625+47roms16@users.noreply.github.com>
2025-04-26 22:46:21 +0800

    style: change text size breakpoint to smaller screen

--- src/components/content/SectionLabel.tsx
+++ src/components/content/SectionLabel.tsx
@@ -6,7 +6,7 @@ function SectionLabel({
   label: string;
 }) {
   return (
-    <p className="font-barlow-condensed text-[1.75rem] tracking-[4px] text-white uppercase max-md:text-center max-md:text-base">
+    <p className="font-barlow-condensed text-[1.75rem] tracking-[4px] text-white uppercase max-md:text-center max-sm:text-base">
       <span className="mr-6 font-bold tracking-[4.72px] text-white/25">
         {numberText}
       </span>
