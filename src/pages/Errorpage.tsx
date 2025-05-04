0d49bed69f67b5f16a78be920853f7db3a66c548
Rommel <137849625+47roms16@users.noreply.github.com.>
2025-05-04 13:39:14 +0800

    feat: add new UI for Error 404 page

--- src/pages/Errorpage.tsx
+++ src/pages/Errorpage.tsx
@@ -1,5 +1,19 @@
 function Errorpage() {
-  return <div>Error</div>;
+  return (
+    <div className="bg-home | text-lavender-blue min-h-dvh content-center bg-cover bg-top text-center max-lg:bg-bottom">
+      <h1 className="font-bellefair text-5xl font-bold">404</h1>
+      <h2 className="font-barlow mb-2 text-2xl">Page Not Found</h2>
+      <p className="font-barlow-condensed mb-16 text-lg">
+        Sorry, the page you are looking for does not exist.
+      </p>
+      <a
+        href="/"
+        className="text-2xl transition-colors before:mr-5 before:content-['<'] hover:text-white"
+      >
+        Go Back Home
+      </a>
+    </div>
+  );
 }
 
 export default Errorpage;
